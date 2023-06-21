0b451dd52e4067487d526a71cb63014db8f8bcbc
PGiang <vophonggiang0205@gmail.com>
2023-06-21 14:30:49 +0700

    push code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "jquery/dist/jquery.min.js"
+import Navbar from './components/Navbar/Navbar'
+import Header from './components/Header/Header';
+import Item from './components/Item/Item';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Header />
+      <Item />
+      <Footer />
     </div>
   );
 }
